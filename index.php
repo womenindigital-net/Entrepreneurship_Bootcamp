2640307aaa1cf1c498ee3ead5d22225bad37fe47
womenindigital-net <94352977+womenindigital-net@users.noreply.github.com>
2022-04-10 15:03:22 +0600

    bootcamp_website

---
+++ index.php
@@ -0,0 +1,400 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
+
+          
+        <!-- Stylesheet -->
+         <!-- Animate css -->
+        <link rel="stylesheet" type="text/css" href="assets/css/animate.css" media="all" />
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+
+        <!-- Fontawesome CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"  crossorigin="anonymous" />
+        <!-- normalize -->
+        <link rel="stylesheet" href="assets/css/normalize.css">
+         <!-- Main Stylesheet -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!-- responsive -->
+        <link rel="stylesheet" href="assets/css/responsive.css">
+
+
+</head>
+ 
+<body>
+
+  <div class="full_wrap">
+    <!-- header section start -->
+    <header class="header_section">
+      <div class="nav_menu">
+        <div class="logo text-center">
+          <a href="#"><img src="assets/images/all-Logo.png" alt="" class="w-25"></a>
+        </div>
+      </div>
+      <div class="bg_image">
+        <img src="assets/images/Bootcamp_web_heading.png" alt="">
+      </div>
+    </header>
+    <!-- header section end -->
+  
+    <!-- program section start  -->
+    <section class="w-100">
+      <div class="">
+        <div class="row m-0">
+          <div class="col-lg-6 m-0 bg p-0">
+            <div class="section_text">
+              <h1>ABOUT THE PROGRAMME</h1>
+              <p>The Entrepreneurship Bootcamp: Ideation to Investment is both an academic course and a competitive training programmed designed to help Participants define their legacy as future entrepreneurs. The one day course, led by industry experts, will stretch the Participants  mind by introducing concepts, frameworks and alternative thinking patterns of an ideal entrepreneur and by teaching how they can drive success with effective product launches. Participants will also compete which will be facilitated by mentors in order to develop their idea into a minimum viable product and present it to investors for a chance to get funded.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-6 m-0 p-0">
+            <div class="section_image">
+              <img src="assets/images/section 1.jpg" alt="image">
+            </div>
+          </div>
+        </div>
+      </div>
+  </section>
+  <!-- program section end  -->
+      
+    <!-- counter section start  -->
+    <section class="counter_area">
+      <div class="container">
+        <div id="countdown">
+          <ul>
+            <li><span id="days"></span>days</li>
+            <li><span id="hours"></span>Hours</li>
+            <li><span id="minutes"></span>Minutes</li>
+            <li><span id="seconds"></span>Seconds</li>
+          </ul>
+        </div>
+        
+        <div class="button">
+          <div class="button_text">
+            <a href="#">Limited seats Avaliable - Apply Today</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- counter section end  -->
+    
+    <!-- unique box section start  -->
+    <section class="c_section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="heading"><h1>WHAT MAKES THIS PROGRAMME TRULY UNIQUE?</h1></div>
+            <div class="all_items_area">
+              <!-- Single box -->
+              <div class="single_item_box">
+                <div class="box_all_content">
+                  <a href="#"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
+                  <h1>Guided Investment</h1>
+                  <p>Guided Investment for best Startup Pitches.</p>
+                </div>
+              </div>
+              <!-- Single box -->
+              <!-- Single box -->
+              <div class="single_item_box">
+                <div class="box_all_content">
+                  <a href="#"><i class="fa fa-plane" aria-hidden="true"></i></a>
+                  <h1>In-person Pitch preparation with Women in Digital</h1>
+                  <p>Final 1 day: intensive in-person pitch preparation, rehearsals and funding.</p>
+                </div>
+              </div>
+              <!-- Single box -->
+              <!-- Single box -->
+              <div class="single_item_box">
+                <div class="box_all_content">
+                  <a href="#"><i class="fa fa-users" aria-hidden="true"></i></a>
+                  <h1>Mentorship & Support</h1>
+                  <p>Live Access to Entrepreneurs, Investors and Accelerators.</p>
+                </div>
+              </div>
+              <!-- Single box -->
+              <!-- Single box -->
+              <div class="single_item_box">
+                <div class="box_all_content">
+                  <a href="#"><i class="fa fa-rocket" aria-hidden="true"></i></a>
+                  <h1>Become a Venture Founder</h1>
+                  <p>Live Access to Entrepreneurs, Investors and Accelerators.</p>
+                </div>
+              </div>
+              <!-- Single box -->
+              <!-- Single box -->
+              <div class="single_item_box">
+                <div class="box_all_content">
+                  <a href="#"><i class="fa fa-microphone" aria-hidden="true"></i></a>
+                  <h1>Pitch to Investors</h1>
+                  <p>Pitching your Big Idea to investors.</p>
+                </div>
+              </div>
+              <!-- Single box -->
+              <!-- Single box -->
+              <div class="single_item_box">
+                <div class="box_all_content">
+                  <a href="#"><i class="fa fa-comments" aria-hidden="true"></i></a>
+                  <h1>LIVE Startup Mentoring</h1>
+                  <p>Deep dive sessions and business plan mentoring for your startup.</p>
+                </div>
+              </div>
+              <!-- Single box -->
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- unique box section end  --> 
+    <!-- course area start   -->
+    <section class="collaps_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="heading2"><h1>COURSE CURRICULUM:</h1></div>
+
+            <div class="accordion" id="accordionExample">
+              <!-- Single item  -->
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                    aria-expanded="true" aria-controls="collapseOne">
+                    20+ MODULES
+                  </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    <ul>
+                      <li><i class="fa fa-transgender" aria-hidden="true"></i>Business Model Canvas</li>
+                      <li><i class="fa fa-television" aria-hidden="true"></i>Presenting with Impact</li>
+                      <li><i class="fa fa-forumbee" aria-hidden="true"></i>Human-centered Design</li>
+                      <li><i class="fa fa-commenting-o" aria-hidden="true"></i>Recruiting and Building Team</li>
+                      <li><i class="fa fa-transgender" aria-hidden="true"></i>Startup Branding</li>
+                      <li><i class="fa fa-television" aria-hidden="true"></i>How Getting Funded Works</li>
+                      <li><i class="fa fa-forumbee" aria-hidden="true"></i>Making Numbers Work</li>
+                      <li><i class="fa fa-commenting-o" aria-hidden="true"></i>Pitch Presentation: Winning against competitors</li>
+                      
+
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <!-- Single item  -->
+              <!-- Single item  -->
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Solutions to your CHALLENGES
+                  </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    <ul>
+                      <li><i class="fa fa-transgender" aria-hidden="true"></i>DISCOVERY: Understand “Becoming an entrepreneur” and “The innovative and creative process”</li>
+                      <li><i class="fa fa-television" aria-hidden="true"></i>VALIDATION: Understand “The Value of MVP” and “Developing a startup on a budget”</li>
+                      <li><i class="fa fa-forumbee" aria-hidden="true"></i>MARKETING: Be able to “Apply Go-To-Market steps” and “Develop sales strategies”</li>
+                      <li><i class="fa fa-commenting-o" aria-hidden="true"></i>FINANCE: Understand “P&Ls, financial modeling” and “how to get your startup investment”</li>
+                      <li><i class="fa fa-transgender" aria-hidden="true"></i>GROWTH: Be able to “Build a winning team” and “Acquire the right talent & leadership strategy”</li>
+                     
+                      
+
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <!-- Single item  -->
+              <!-- Single item  -->
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    And much more to make you SUCCESSFUL!
+                  </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    <ul>
+                      <li><i class="fa fa-transgender" aria-hidden="true"></i>Series of Guest Lectures by leading Investors</li>
+                      <li><i class="fa fa-television" aria-hidden="true"></i>Business plan & investment pitch preparation</li>
+                      <li><i class="fa fa-forumbee" aria-hidden="true"></i>Business idea & Investment Summit</li>
+                      <li><i class="fa fa-commenting-o" aria-hidden="true"></i>Pitch Practice Sessions</li>
+                      
+
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <!-- Single item  -->
+              
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- course area end   -->
+
+    <!-- why bootcamp section start  -->
+    <section class="why_bootcamp_area">
+      <div class="container-fluid p-0">
+        <div class="row">
+          <div class="col-lg-6 m-0 bg_1 p-0 d-flex align-items-center">
+            <div class="camp_text">
+              <h1>WHY ENROL ON THE ENTREPRENEURSHIP BOOTCAMP?</h1>
+              <div class="part">
+                <div class="icon"><i class="fa fa-rocket" aria-hidden="true"></i></div>
+                <p>Our mission is to create entrepreneurs and leaders of tomorrow.
+                  Our programme is truly unique: We offer access to our ecosystem of investors, incubation hubs,
+                  and successful entrepreneurs for all participants and guaranteed investment to the  pitches.</p>
+              </div>
+              <div class="part">
+                <div class="icon"><i class="fa fa-star" aria-hidden="true"></i></div>
+                <p>Innovation is our forte. We are fortunate to bring these world-class experts to the boot-camp and provide an entrepreneurial environment for the great ventures of tomorrow!</p>
+              </div>
+              
+            </div>
+          </div>
+          <div class="col-lg-6 p-0 d-flex justify-content-center bg_1">
+            <div class="section_image">
+              <img src="assets/images/section 2.jpg" alt="image">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- why bootcamp section end  -->
+    
+    <!-- after bootcamp section start  -->
+    <section class="after_bootcamp">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="after_bootcamp_text">
+              <h1>WHAT WORLD LEADING EXPERTS SAY ABOUT ENTREPRENEURSHIP BOOTCAMP</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- after bootcamp section end  -->
+
+    <!-- section start  -->
+    <section class="enrol_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="enrol_text">
+              <h1>ENROL Today</h1>
+              <P>To be the best, you must learn </P>
+              <div class="button">
+                <a href="#">Apply now</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- section end  -->
+    <!-- section start  -->
+    <footer id="footer">
+      <div class="footer-top p_70">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 footer-contact">
+              <img src="assets/images/WiD-white-Logo.svg" alt="wid-logo" class="mb-3" height="50px">
+              <p>
+                3rd Floor, Y-13, Nur Jahan Road, Mohammadpur, Dhaka 1207<br>
+                <br>
+                <strong>Phone: </strong>+8801635-497868<br>
+                <strong>Email: </strong>womenindigital.net@gmail.com<br>
+              </p>
+            </div>
+            <div class="col-lg-2 col-md-6 col-sm-6 col-6 footer-links">
+              <h4>Useful Links</h4>
+              <ul>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="https://womenindigital.net">Home</a></li>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="https://womenindigital.net/about-us">About us</a></li>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Services</a></li>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Terms of service</a></li>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i><a href="https://womenindigital.net/policy">Privacy policy</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-6 footer-links">
+              <h4>Our Services</h4>
+              <ul>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Web Design</a></li>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Web Development</a></li>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Product Management</a></li>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Marketing</a></li>
+                <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Graphic Design</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 col-item footer-newsletter">
+              <h4>Join Our Newsletter</h4>
+              <!-- <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p> -->
+              <form action="https://womenindigital.net/subscribe" method="POST" class="mt-3">
+                <input type="hidden" name="_token" value="NcMz9ZAMQODimR0lHEzH1R9gx3AIPCnXVVCzjNSm">              <div class="input-group">
+                  <input type="email" name="email" class="form-control" placeholder="example@gmail.com" required="">
+                  <button type="submit" class="btn primary-btn px-3"><i class="bi bi-send"></i></button>
+                </div>
+              </form>
+              <div class="social-links text-center text-md-right pt-3">
+                <a href="https://twitter.com/wid_network" class="twitter" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></i></a>
+                <a href="https://www.facebook.com/WomeninDigital.net/" class="facebook" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/womenindigital_net/" class="instagram" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://bd.linkedin.com/company/womenindigitalnet" class="linkedin" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container py-4">
+        <div class="d-flex flex-column flex-md-row text-center justify-content-md-between">
+          <div class="copyright pb-3 pb-md-0">
+            <strong><span>© 2013-2022 Women In Digital</span></strong>. All Rights Reserved
+          </div>
+          <div>
+            Design And Developed By <a class="w-text-primary" href="https://womenindigital.net"><strong>Women In Digital</strong></a> | <a href="http://luminadev.com/" class="text-warning"><strong>Lumina Dev</strong></a>
+          </div>
+        </div>
+      </div>
+  </footer>
+    <!-- section end  -->
+  
+  
+  </div>
+
+  <!-- js files -->
+  <!-- bootstrap pooper -->
+  <script type="text/javascript" src="assets/js/popper.min.js"></script>
+
+  <!-- bootstrap -->
+  <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <!-- jquary -->
+  <script type="text/javascript" src="assets/js/vendor/jquery-3.2.1.min.js"></script>
+  <!-- custom script -->
+  <script type="text/javascript" src="assets/js/main.js"></script>
+
+
+</body>
+
+</html>
